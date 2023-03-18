2ede4625ce2f5a1ae6f176e3ff5e8b45b8b0badf
Lancelot Normand <lancelotnormand@outlook.com>
2023-03-18 13:24:40 -0400

    removed oussama

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.ensemble import RandomForestRegressor
 
-# Charger l'ensemble de données
-data = pd.read_parquet('NORWAY_INFLOW_2018-2022.parquet')
-
-# Prétraiter les données
-data['Time_id'] = data['Year'].astype(str) + '-' + data['Week'].astype(str)
-data['Time_id'] = pd.to_datetime(data['Time_id'] + '-1', format='%Y-%W-%w')
-
+PARQUET = "NORWAY_INFLOW_2018-2022.parquet"
 
 def main():
     print("Hello")
+    df = pd.read_parquet(PARQUET)
+    df.to_csv("parquet.csv")
 
 
 if __name__ == "__main__":
