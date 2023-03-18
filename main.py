0a119ecbf42240f9e37bc3ca9e5a43dcd8ad4c06
Lancelot Normand <lancelotnormand@outlook.com>
2023-03-18 13:22:36 -0400

    file csv

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-
-
+PARQUET = "NORWAY_INFLOW_2018-2022.parquet"
 
 def main():
     print("Hello")
+    df = pd.read_parquet(PARQUET)
+    df.to_csv("parquet.csv")
 
 
 if __name__ == "__main__":
