2f925f3b2b00774c9f994e6468780b7506682489
oussamelfig <oelfigha2002@gmail.com>
2023-03-18 13:18:16 -0400

    charger les donnees

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
 
+# Charger l'ensemble de données
+data = pd.read_parquet('NORWAY_INFLOW_2018-2022.parquet')
 
+# Prétraiter les données
+data['Time_id'] = data['Year'].astype(str) + '-' + data['Week'].astype(str)
+data['Time_id'] = pd.to_datetime(data['Time_id'] + '-1', format='%Y-%W-%w')
 
 
 def main():
